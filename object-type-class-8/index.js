f33337af405642c3aa0c2af29321c17e57474887
azim <mdazimmia53@gmail.com>
2024-04-09 09:51:42 +0600

    add file

---
+++ object-type-class-8/index.js
@@ -0,0 +1,11 @@
+// object type discribtion
+var users;
+users = [];
+var user1;
+user1 = { name: "azim", userId: 101 };
+users.push(user1);
+//step 2...
+var user2;
+user2 = { name: "karim", userId: 102 };
+users.push(user2);
+console.log(users);
---
+++ object-type-class-8/index.ts
@@ -0,0 +1,12 @@
+// object type discribtion
+let users:object[]
+users=[];
+
+let user1:{name:string,userId:number};
+user1={name:"azim", userId:101}
+users.push(user1)
+//step 2...
+let user2:{name:string, userId:number};
+user2={name:"karim", userId:102};
+users.push(user2);
+console.log(users);
